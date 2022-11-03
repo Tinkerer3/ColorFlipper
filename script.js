1046671655813229782094d299904ccd1bc971b7
Adem Yıldırım <91284855+Tinkerer3@users.noreply.github.com>
2022-11-03 20:45:45 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let colorName = document.getElementById("colorName");
+let normalColorNames = ["Salmon","Gold","LawnGreen","DarkCyan","DarkTurquoise","Olive"];
+let hexColorNames = ["#B8860B", "#800000","#00BFFF","#9ACD32","#F0E68C","#FF4500"]
+isHax = false;
+
+function haxOn(){
+    isHax = true;
+}
+
+function haxOff(){
+    isHax = false;
+}
+
+function onClick(){
+    if (isHax){
+    let item = hexColorNames[Math.floor(Math.random()*hexColorNames.length)];
+    document.body.style.background = item;
+    colorName.innerHTML = item; 
+    }
+    else{
+    let item = normalColorNames[Math.floor(Math.random()*normalColorNames.length)];
+    document.body.style.background = item;
+    colorName.innerHTML = item;
+    }
+    
+}
