fae746ab00d22931de4308cb65c90b7513a57d39
Adem Yıldırım <91284855+Tinkerer3@users.noreply.github.com>
2022-11-03 22:38:31 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let colorName = document.getElementById("colorName");
 let normalColorNames = ["Salmon","Gold","LawnGreen","DarkCyan","DarkTurquoise","Olive"];
 let hexColorNames = ["#B8860B", "#800000","#00BFFF","#9ACD32","#F0E68C","#FF4500"]
+let textInput = document.getElementById("text-input")
 isHax = false;
 
 function haxOn(){
@@ -23,4 +24,9 @@ function onClick(){
     colorName.innerHTML = item;
     }
     
+}
+
+function onClickInput(){
+    document.body.style.background =`#${textInput.value}` ;
+    colorName.innerHTML = textInput.value;
 }
